fa2078564180ba2c76ea3bd448d0ab0430837405
Damian Williamson <damian@willtech.com.au>
2012-10-10 22:22:42 +1100

    Initial Commit for branch: reiterator
    Signed-off-by: Damian Williamson <damian@willtech.com.au>

---
+++ reiterator/reiterate.php
@@ -0,0 +1,25 @@
+<?php
+
+#####################################################################
+#####################################################################
+
+// /pub-utils/reiterator/reiterate.php - a re-numbering iterator
+//
+// Utility to iterate over a file input as a string and
+// replacing after $b which is before $e and  with $n++;
+
+// NOTICE
+// ======
+// Per detail in the included LICENCE this work Public Utilities
+// by Willtech is licensed under the
+// Creative Commons Attribution-ShareAlike 3.0 Unported License
+// Based on a work at git://github.com/Willtech/pub-utils.git
+// To view a copy of this license, visit:
+// http://creativecommons.org/licenses/by-sa/3.0/.
+// Both this NOTICE and the LICENCE must be included with every
+// file in any distribution.
+
+## placeholder
+
+?>
+
