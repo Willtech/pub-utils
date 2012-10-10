394228bf588b846f547e102186db5bacfc8a5a73
Damian Williamson <damian@willtech.com.au>
2012-10-10 22:30:47 +1100

    Initial Commit for branch: smtp
    Signed-off-by: Damian Williamson <damian@willtech.com.au>

---
+++ smtp/smtp-email.php
@@ -0,0 +1,23 @@
+<?php
+
+#####################################################################
+#####################################################################
+
+// /pub-utils/pear-smtp/smpt-email.php - an smtp client for php
+//
+
+// NOTICE
+// ======
+// Per detail in the included LICENCE this work Public Utilities
+// by Willtech is licensed under the
+// Creative Commons Attribution-ShareAlike 3.0 Unported License
+// Based on a work at git://github.com/Willtech/pub-utils.git
+// To view a copy of this license, visit:
+// http://creativecommons.org/licenses/by-sa/3.0/.
+// Both this NOTICE and the LICENCE must be included with every
+// file in any distribution.
+
+## placeholder
+
+?>
+
